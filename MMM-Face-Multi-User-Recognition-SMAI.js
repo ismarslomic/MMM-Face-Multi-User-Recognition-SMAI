1ad3713ba7ccc381abceaab20dfe063214c4a05c
Ismar Slomic <ismar.slomic@ruter.no>
2021-03-23 21:33:29 +0100

    remove grunt (deprecated), clean up unused config and update readme

--- MMM-Face-Multi-User-Recognition-SMAI.js
+++ MMM-Face-Multi-User-Recognition-SMAI.js
@@ -3,173 +3,155 @@
 /* Magic Mirror
  * Module: MMM-Face-Multi-User-Recognition-SMAI
  *
- * By James Macdonald
+ * Extended by Ismar Slomic (originally by Eben Kouaos and James Macdonald)
  * MIT Licensed.
  */
 
-const MORNING   = -1;
+const MORNING = -1;
 const AFTERNOON = 0;
-const EVENING   = 1;
+const EVENING = 1;
 Module.register("MMM-Face-Multi-User-Recognition-SMAI", {
-	defaults: {
-		updateInterval: 10000,
-		retryDelay: 5000,
-		width: "200px",
-		position: "lower_third",
-		useMMMFaceRecoDNN: false,
-		interval: 2000,
-		morningStartTime: 3,
-		morningEndTime: 12,
-		afternoonStartTime: 12,
-		afternoonEndTime: 17,
-	},
+  defaults: {
+    useMMMFaceRecoDNN: true,
+    width: "200px",
+    morningStartTime: 3,
+    morningEndTime: 12,
+    afternoonStartTime: 12,
+    afternoonEndTime: 17,
+  },
 
-	requiresVersion: "2.1.0", // Required version of MagicMirror
+  requiresVersion: "2.1.0", // Required version of MagicMirror
 
-	start: function() {
-		var self = this;
-		var dataRequest = null;
-		var dataNotification = null;
-		this.userName = "Guest";
-		this.userImage = "guest.gif";
-		this.timer = null;
+  start: function () {
+    var self = this;
+    var dataRequest = null;
+    var dataNotification = null;
+    this.userName = "Guest";
+    this.userImage = "guest.gif";
+    this.timer = null;
 
-		// Are we logged in or not
-		this.loggedIn = false;
+    // Are we logged in or not
+    this.loggedIn = false;
 
-		//Flag for check if module is loaded
-		this.loaded = false;
-	},
+    //Flag for check if module is loaded
+    this.loaded = false;
+  },
 
-	// This will be called every update and update the image based on:
-	//	.userId
-	//	.userName
-	getDom: function() {
-		var self = this;
+  // This will be called every update and update the image based on:
+  //	.userId
+  //	.userName
+  getDom: function () {
+    var self = this;
 
-		// create element wrapper for show into the module
-		var wrapper = document.createElement("div");
-		wrapper.innerHTML = this.translate('TITLE');
-		wrapper.className = "face-image";
+    // create element wrapper for show into the module
+    var wrapper = document.createElement("div");
+    wrapper.innerHTML = this.translate('TITLE');
+    wrapper.className = "face-image";
 
-		// Figure out what time of day message we want
-		var message = this.translate('WELCOME');
-		if ( this.userName !== "Guest")
-		{
-			if ( this.timeOfDay() == MORNING ) {
-				message = this.translate('GOOD_MORNING');
-			} else if (this.timeOfDay() == AFTERNOON) {
-				message = this.translate('GOOD_AFTERNOON');
-			} else {
-				message = this.translate('GOOD_EVENING');
-			}
-		}
-		
-		// Ceate the image element and show gif by default.
-		var imgHolderElement = document.createElement("p");
-		imgHolderElement.innerHTML = message + " " + this.capitalizeWords(this.userName);
-		imgHolderElement.classList.add(this.config.position);
-		imgHolderElement.style.width = this.config.width;
-		
-		// Asychronoulsy load either GIF or face (as name).
-		var img = document.createElement("img");
-		var newImg = new Image;
-		newImg.src = "modules/MMM-Face-Multi-User-Recognition-SMAI/public/" + this.userImage;
-		newImg.onload = function()
-		{
-			img.src = this.src;
-		}
-		imgHolderElement.appendChild(img);
+    // Figure out what time of day message we want
+    var message = this.translate('WELCOME');
+    if (this.userName !== "Guest") {
+      if (this.timeOfDay() == MORNING) {
+        message = this.translate('GOOD_MORNING');
+      } else if (this.timeOfDay() == AFTERNOON) {
+        message = this.translate('GOOD_AFTERNOON');
+      } else {
+        message = this.translate('GOOD_EVENING');
+      }
+    }
 
-		wrapper.appendChild(imgHolderElement);
-		return wrapper;
+    // Ceate the image element and show gif by default.
+    var imgHolderElement = document.createElement("p");
+    imgHolderElement.innerHTML = message + " " + this.capitalizeWords(
+        this.userName);
+    imgHolderElement.style.width = this.config.width;
 
-	},
-	
-	getScripts: function() {
-		return [];
-	},
+    // Asychronoulsy load either GIF or face (as name).
+    var img = document.createElement("img");
+    var newImg = new Image;
+    newImg.src = "modules/MMM-Face-Multi-User-Recognition-SMAI/public/"
+        + this.userImage;
+    newImg.onload = function () {
+      img.src = this.src;
+    }
+    imgHolderElement.appendChild(img);
 
-	getStyles: function () {
-		return [
-			"MMM-Face-Multi-User-Recognition-SMAI.css",
-		];
-	},
+    wrapper.appendChild(imgHolderElement);
+    return wrapper;
 
-	// Load translations files
-	getTranslations: function() {
-		//FIXME: This can be load a one file javascript definition
-		return {
-			en: "translations/en.json",
-			es: "translations/es.json",
-			ko: "translations/ko.json"
-		};
-	},
+  },
 
-	notificationReceived: function(notification, payload, sender) {
-		var self = this;
+  getScripts: function () {
+    return [];
+  },
 
-		// Log.log("Got Notificaiton: " + notification + " Payload: " + payload);
-		switch (notification)
-		{
-			case "USERS_LOGIN":
-			{
-				// Face Rec sends multiple notifications even if user is already logged in and logout timer still active.
-				if (this.config.useMMMFaceRecoDNN === true && this.loggedIn == false )
-				{
-					Log.log("Notificaiton: " + notification + " from Mirror. Logging in " + payload);
-					
-					// Fetch the users image.
-					this.loggedIn = true;
-					this.userName = payload;
-					this.userImage = payload + ".jpg"; //Assume for now.
-					self.updateDom(100);
+  getStyles: function () {
+    return [
+      "MMM-Face-Multi-User-Recognition-SMAI.css",
+    ];
+  },
 
-					// Clear existing timer and reset.
-					// This is only required if MMM-Face-Reco-DNN module not updated with new Notification for lougout.
-//					if ( this.timer != null ) clearInterval(this.timer);
-//					this.timer = setTimeout(()=>{
-//						Log.log("Logging out. Reset image.");
-//						
-//						this.loggedIn = false;
-//						this.userName = "Mr. Nobody";
-//						this.userImage = "guest.gif";
-//						self.updateDom(100);
-//					}, 25000);
-				}
-				break;
-			}
-			case "USERS_LOGOUT_MODULES":
-			{
-				Log.log("Notificaiton: " + notification + " from Mirror. Logging out " + payload);
-				
-				this.loggedIn = false;
-				this.userName = "Mr. Nobody";
-				this.userImage = "guest.gif";
-				self.updateDom(100);
-				
-				break;
-			}
-		}		
-	},
+  // Load translations files
+  getTranslations: function () {
+    //FIXME: This can be load a one file javascript definition
+    return {
+      en: "translations/en.json",
+      es: "translations/es.json",
+      ko: "translations/ko.json"
+    };
+  },
 
-	// Local helper functions
-	capitalizeWords: function(str)
-	{
-	 return str.toString().toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.substring(1)).join(' ');
-	},
-	
-	timeOfDay: function()
-	{
-		var hour = moment().hour();
-		
-		if (hour >= this.config.morningStartTime && hour < this.config.morningEndTime) {
-			return MORNING;
-		} else if (hour >= this.config.afternoonStartTime && hour < this.config.afternoonEndTime ) {
-			return AFTERNOON;
-		}
+  notificationReceived: function (notification, payload, sender) {
+    var self = this;
 
-		return EVENING;
-	}
-	
+    // Log.log("Got Notificaiton: " + notification + " Payload: " + payload);
+    switch (notification) {
+      case "USERS_LOGIN": {
+        // Face Rec sends multiple notifications even if user is already logged in and logout timer still active.
+        if (this.config.useMMMFaceRecoDNN === true && this.loggedIn == false) {
+          Log.log("Notificaiton: " + notification + " from Mirror. Logging in "
+              + payload);
+
+          // Fetch the users image.
+          this.loggedIn = true;
+          this.userName = payload;
+          this.userImage = payload + ".jpg"; //Assume for now.
+          self.updateDom(100);
+        }
+        break;
+      }
+      case "USERS_LOGOUT_MODULES": {
+        Log.log("Notificaiton: " + notification + " from Mirror. Logging out "
+            + payload);
+
+        this.loggedIn = false;
+        this.userName = "Mr. Nobody";
+        this.userImage = "guest.gif";
+        self.updateDom(100);
+
+        break;
+      }
+    }
+  },
+
+  // Local helper functions
+  capitalizeWords: function (str) {
+    return str.toString().toLowerCase().split(' ').map(
+        word => word.charAt(0).toUpperCase() + word.substring(1)).join(' ');
+  },
+
+  timeOfDay: function () {
+    var hour = moment().hour();
+
+    if (hour >= this.config.morningStartTime && hour
+        < this.config.morningEndTime) {
+      return MORNING;
+    } else if (hour >= this.config.afternoonStartTime && hour
+        < this.config.afternoonEndTime) {
+      return AFTERNOON;
+    }
+
+    return EVENING;
+  }
+
 });
--- README.md
+++ README.md
@@ -1,63 +1,79 @@
 # MMM-Face-Multi-User-Recognition-SMAI
-This is a module for the [MagicMirror²](https://github.com/MichMich/MagicMirror/) to add face recognition.
-This module uses [OpenCV face-recognition with DNN](https://github.com/nischi/MMM-Face-Reco-DNN) to handle the user profile recognition. It will show/hide modules based on classes and can handle multiple users.
+This is a module for the [MagicMirror²](https://github.com/MichMich/MagicMirror/) to add face 
+recognition. This module uses [OpenCV face-recognition with DNN](https://github.com/nischi/MMM-Face-Reco-DNN) 
+to handle the user profile recognition. It will show/hide modules based on classes and can handle 
+multiple users.
 
+It is a fork of James Macdonalds [MMM-Face-Multi-User-Recognition-SMAI](https://github.com/jimbydude/MMM-Face-Multi-User-Recognition-SMAI) 
+which again is fork of Eben Kouaos [https://github.com/EbenKouao/MMM-Face-Recognition-SMAI](https://github.com/EbenKouao/MMM-Face-Recognition-SMAI)
+(see his other projects at https://smartbuilds.io/).
 
-It is a fork of Eben Kouaos https://github.com/EbenKouao/MMM-Face-Recognition-SMAI. See his other projects at https://smartbuilds.io/
+Reason for making another fork is lack of maintenance of the other two repos, see my comment at
+[MMM-Face-Recognition-SMAI/issues/5](https://github.com/EbenKouao/MMM-Face-Recognition-SMAI/issues/5#issuecomment-805166266)
 
 ## How it works
-This module allows you to access profiles using face recognition. This works on the back of MMM-Face-Reco-DNN/OpenCV face recognition module.
+This module allows you to access profiles using face recognition. This works on the back of 
+[MMM-Face-Reco-DNN/OpenCV](https://github.com/nischi/MMM-Face-Reco-DNN#opencv) face recognition module.
 
-When a user is detected and logged in by Face Recognition Module a notification is recieved that will trigger the users profile pic ot be displayed
-until the user is logged out by MMM-Face-Reco-DNN
+When a user is detected and logged in by Face Recognition Module a notification is received that 
+will trigger the users profile pic ot be displayed until the user is logged out by 
+[MMM-Face-Reco-DNN](https://github.com/nischi/MMM-Face-Reco-DNN)
 
 ## Screenshots
 | ![FaceID Guest](img/readme/face-recognition-guest-smai.png) | ![Face ID Detected](img/readme/face-recognition-stark-smai.png) | 
 |---|---|
 | A guest profile as default | User has been recognised |
 
-'Screenshots from Ebens Module'
+* Screenshots from [https://github.com/EbenKouao/MMM-Face-Recognition-SMAI](https://github.com/EbenKouao/MMM-Face-Recognition-SMAI)
 
 ## Preconditions
 
 * MagicMirror<sup>2</sup> instance
 * Node.js version >= 7
 * npm
-* [OpenCV face-recognition with DNN](https://github.com/nischi/MMM-Face-Reco-DNN)
+* [MMM-Face-Reco-DNN](https://github.com/nischi/MMM-Face-Reco-DNN)
 * Raspberry Pi 4 Camera Module
 
-Make sure to complete MMM-Face-Reco-DNN setup including adding classes for showing and hiding to your config before proceding.
+Make sure to complete [MMM-Face-Reco-DNN](https://github.com/nischi/MMM-Face-Reco-DNN) setup 
+including adding classes for showing and hiding to your config before proceeding.
 
 ## Step 1 – Install the module
 In your MagicMirror directory:
 
-```bash cd modules
-   git clone https://github.com/jimbydude/MMM-Face-Multi-User-Recognition-SMAI.git
-   cd MMM-Face-Multi-User-Recognition-SMAI
-   npm install
+```bash 
+cd modules
+git clone https://github.com/ismarslomic/MMM-Face-Multi-User-Recognition-SMAI
+cd MMM-Face-Multi-User-Recognition-SMAI
+npm install
 ```
 
 ## Step 2 – Add files to the Config.js
 Here is an example for an entry in `config.js`
 
-```javascript
+```json
 {
   module: "MMM-Face-Multi-User-Recognition-SMAI",
   position: "top_right",
   config: {
-    useMMMFaceRecoDNN: true
+    useMMMFaceRecoDNN: true,
+    width: "200px",
+    morningStartTime: 3,
+    morningEndTime: 12,
+    afternoonStartTime: 12,
+    afternoonEndTime: 17,
   }
 },
 ```
 
 ## Step 3 – Configuring the Face Recognition Structure
-**Pre-requisite:** Follow isntructions in OpenCV Installation to setup your dataset with images.
+**Pre-requisite:** Follow instructions in OpenCV Installation to setup your dataset with images.
 
 This module assumes a few things:
-* The default gif is in 'Public' folder
-* There is an image for each user in the 'Public' folder that matches with user directory names in OpenCV Dataset
+* The image file `guest.gif` for guest users is in the `public` folder
+* There is an image for each (known) user in the `public` folder that matches with user directory names 
+  in OpenCV dataset
 
-For example if you have the following structure containing your image set in OpenCV module:
+For example if you have the following structure containing your image as dataset in OpenCV module:
 ```
 - dataset
   - james
@@ -70,7 +86,7 @@ For example if you have the following structure containing your image set in Ope
     - img03.jpg
 ```
 
-Then you will also have the following in the 'Public' folder of this module:
+Then you will also have the following in the `public` folder of this module:
 ```
 - public
     - guest.gif
@@ -78,10 +94,25 @@ Then you will also have the following in the 'Public' folder of this module:
     - roseann.jpg
 ```
 
-These images are the ones that will be shown on the mirror UI when a user logs in, not the ones from dataset. Those are only to train the NN.
+These images are the ones that will be shown on the mirror UI when a user logs in, 
+not the ones from OpenCV dataset. Images in the OpenCV dataset are only to train the NN.
+
+NOTE! Only images with extension `.jpg` are supported currently in this module
+
+## Greetings message
+Greetings message is displayed just above the user image, based of the time of the day.
+
+- Between `morningStartTime` and `morningEndTime`: `"Good Morning <name of the user>"`
+- Between `afternoonStartTime` and `afternoonEndTime`: `"Good Afternoon <name of the user>"`
+- Else: `"Good Evening <name of the user>"`
 
 ## Configuration options
 
-| Option           | Description
-|----------------- |-----------
-| `useMMMFaceRecoDNN`        | *Required* Use the DNN deetction type. Currently only supported type. <br><br>**Type:** `bool` <br>Default true
+| Option                      | Description
+|---------------------------- |-----------
+| `useMMMFaceRecoDNN`         | *Required* Use the DNN detection type. Currently only supported type. <br><br>**Type:** `bool` <br>**Default Value**: `true`
+| `width`                     | The width of the HTML image container. <br><br>**Type:** `string` <br>**Default Value**: `200px`
+| `morningStartTime`          | Hour when morning start, used for greetings message. <br><br>**Type:** `int` <br>**Default Value**: `3`
+| `morningEndTime`            | Hour when morning ends, used for greetings message. <br><br>**Type:** `int` <br>**Default Value**: `12`
+| `afternoonStartTime`        | Hour when afternoon starts, used for greetings message. <br><br>**Type:** `int` <br>**Default Value**: `12`
+| `afternoonEndTime`          | Hour when afternoon ends, used for greetings message. <br><br>**Type:** `int` <br>**Default Value**: `17`
